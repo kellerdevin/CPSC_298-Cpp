08dd93beb9e416230bd4ec8146379b75446c4379
Devin Keller <dkeller@chapman.edu>
2021-01-17 11:12:32 -0800

    added comments

--- Assignment1/cereal.cpp
+++ Assignment1/cereal.cpp
@@ -5,6 +5,7 @@ int main()
 {
     const double TON = 35273.92;
 
+    // Grab cereal weight from user
     cout << "Enter cereal weight in ounces: ";
 
 
@@ -12,6 +13,8 @@ int main()
     cin >> cereal;
 
     float weight;
+
+    //Calculate weight in tons
     weight = cereal / TON;
 
     cout << "The cereal is " << weight << " metric tons";
