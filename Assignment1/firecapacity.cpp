2fcc6a0a5dc44395d6110f2f0640df9faaac1609
Devin Keller <dkeller@chapman.edu>
2021-01-17 11:16:40 -0800

    Added ccomments

--- Assignment1/firecapacity.cpp
+++ Assignment1/firecapacity.cpp
@@ -6,13 +6,17 @@ int main()
 {
 cout << endl;
 int maxCapacity;
+
+//Get max room capacity from user
 cout << "Enter max room capacity: ";
 cin >> maxCapacity;
 
+//Get number of people attending meeting
 int numPeople;
 cout << "Enter number of people attending the meeting: ";
 cin >> numPeople;
 cout << endl;
+//Calculate how many more people you can add
 int additionalPeople = maxCapacity - numPeople;
 
 if (numPeople <= maxCapacity){
