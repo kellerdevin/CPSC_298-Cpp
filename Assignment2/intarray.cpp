66f0d1e5ed5f836f1c1f7f57668b4e49c1ccf97c
Devin Keller <dkeller@chapman.edu>
2021-01-23 14:37:42 -0800

    Added NUM_ELEMENTS Array

---
+++ Assignment2/intarray.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // read 10 nonnegative integers from user into array numberArray
+
+    const int NUM_ELEMENTS = 10;
+    int numberArray[NUM_ELEMENTS]; 
+    unsigned int i;
+
+    cout << NUM_ELEMENTS << endl;
+
+    /* for (i = 0; i < numberArray.size(); ++i)
+    {
+        cin >>  numberArray.at(i);
+    }
+
+    cout << "Enter 5 integers: " << endl;
+
+
+    //print integers back to screen
+
+    return 0;
+    */
+
+}
